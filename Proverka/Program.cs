9dce552e02edb952ff0bd9f95423772c6963a1ac
Rmpol <polyakov.ramiz@gmail.com>
2023-05-31 01:34:21 +0500

    Добавил
    обновил

--- Proverka/Program.cs
+++ Proverka/Program.cs
@@ -2,14 +2,9 @@
 using iTextSharp.text.pdf.parser;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Proverka
 {
@@ -18,16 +13,26 @@ namespace Proverka
         static void Main(string[] args)
         {
             string filePath = @"C:\Users\User\Desktop\33.pdf";
+
+            //Получаем коллекцию состоящую из строк со всех страниц pdf файла
             List<string> mergeLinesAllPages = ExtractInformationFromPdfFile(filePath);
+
+            //Получаем коллекцию без лишней информации
             List<string> experience = GetExperience(mergeLinesAllPages);
 
-            // Выводим значения коллекции experience
-            for (int i = 0; i < experience.Count; i++)
-            {
-                Console.WriteLine("Index: " + i);
-                Console.WriteLine("Value: " + experience[i]);
-            }
+            //удаляем все элементы со значением "Резюме обновлено"
+            RemoveResumeUpdates(experience);
+
+            //Получаем общий стаж работы
+            string totalWorkExperience = ProcessExperience(experience[0], experience);
+
+            GetTimeAndPlaceOfWork(experience);
+
+
+            //Console.WriteLine(experience);
         }
+
+
 
         static List<string> ExtractInformationFromPdfFile(string filePath)
         {
@@ -83,6 +88,131 @@ namespace Proverka
             }
 
             return experience;
+        }
+
+        static string ProcessExperience(string experienceLine, List<string> experience)
+        {
+            string totalWorkExperience = "";
+
+            if (experienceLine.StartsWith("Опыт работы"))
+            {
+                int startIndex = FindFirstDigitIndex(experienceLine);
+
+                if (startIndex != -1 && startIndex < experienceLine.Length)
+                {
+                    totalWorkExperience = experienceLine.Substring(startIndex);
+                }
+            }
+
+            // Удаляем строки из списка experience
+            experience = RemoveLines(experience, 0, 1);
+
+            return totalWorkExperience;
+        }
+
+        static int FindFirstDigitIndex(string line)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (char.IsDigit(line[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static List<string> RemoveResumeUpdates(List<string> experience)
+        {
+            List<string> itemsToRemove = experience.Where(item => item.StartsWith("Резюме обновлено")).ToList();
+            if (itemsToRemove.Count > 0)
+            {
+                foreach (string item in itemsToRemove)
+                {
+                    experience.Remove(item);
+                }
+            }
+            return experience;
+        }
+
+
+        static List<string> RemoveLines(List<string> lines, int startIndex, int count)
+        {
+            lines.RemoveRange(startIndex, count);
+            return lines;
+        }
+
+        public static void GetTimeAndPlaceOfWorkAll(List<string> experience, List<string> informationOfWork)
+        {
+            if (experience.Count >= 4)
+            {
+                string startWork = experience[0];
+                string companyName = experience[1];
+                string endWork = experience[2];
+                string generalInformation = string.Empty;
+
+                int endIndex = FindYearMonthIndex(experience);
+                if (endIndex < 0)
+                {
+                    endIndex = experience.Count;
+                }
+
+                generalInformation = string.Join(" ", experience.GetRange(3, endIndex - 3));
+
+                informationOfWork.Add(startWork);
+                informationOfWork.Add(companyName);
+                informationOfWork.Add(endWork);
+                informationOfWork.Add(generalInformation);
+
+                Console.WriteLine("Information of Work:");
+                foreach (string info in informationOfWork)
+                {
+                    Console.WriteLine(info);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно данных для обработки опыта работы.");
+            }
+        }
+
+        public static void GetTimeAndPlaceOfWork(List<List<string>> experiences)
+        {
+            int counter = 0;
+            foreach (List<string> experience in experiences)
+            {
+                List<string> informationOfWork = new List<string>();
+                string collectionName = "informationOfWork";
+                if (counter > 0)
+                {
+                    collectionName += "_" + counter;
+                }
+
+                Console.WriteLine("Processing " + collectionName + ":");
+                GetTimeAndPlaceOfWorkAll(experience, informationOfWork);
+
+                // Здесь вы можете выполнять операции с коллекцией informationOfWork,
+                // сохранять ее или использовать по своему усмотрению
+
+                counter++;
+                Console.WriteLine();
+            }
+        }
+
+        public static int FindYearMonthIndex(List<string> experience)
+        {
+            Regex regex = new Regex(@"^[А-Яа-я]+\s\d{4}");
+
+            for (int i = 3; i < experience.Count; i++)
+            {
+                if (regex.IsMatch(experience[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
 
     }
